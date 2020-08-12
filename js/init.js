b1ff61efbfdb64e1ddf1414c72b09e2b79447401
Jacky Tsang <jkythc@gmail.com>
2020-08-12 14:29:34 +0800

    Update js

--- js/init.js
+++ js/init.js
@@ -10,9 +10,9 @@
 /* FitText Settings
 ------------------------------------------------------ */
 
-    setTimeout(function() {
-	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
-	 }, 100);
+   //  setTimeout(function() {
+	//    $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
+	//  }, 100);
 
 
 /*----------------------------------------------------*/
@@ -102,54 +102,54 @@
 /*	Modal Popup
 ------------------------------------------------------*/
 
-    $('.item-wrap a').magnificPopup({
+   //  $('.item-wrap a').magnificPopup({
 
-       type:'inline',
-       fixedContentPos: false,
-       removalDelay: 200,
-       showCloseBtn: false,
-       mainClass: 'mfp-fade'
+   //     type:'inline',
+   //     fixedContentPos: false,
+   //     removalDelay: 200,
+   //     showCloseBtn: false,
+   //     mainClass: 'mfp-fade'
 
-    });
+   //  });
 
-    $(document).on('click', '.popup-modal-dismiss', function (e) {
-    		e.preventDefault();
-    		$.magnificPopup.close();
-    });
+   //  $(document).on('click', '.popup-modal-dismiss', function (e) {
+   //  		e.preventDefault();
+   //  		$.magnificPopup.close();
+   //  });
 
 
 /*----------------------------------------------------*/
 /*	Flexslider
 /*----------------------------------------------------*/
-   $('.flexslider').flexslider({
-      namespace: "flex-",
-      controlsContainer: ".flex-container",
-      animation: 'slide',
-      controlNav: true,
-      directionNav: false,
-      smoothHeight: true,
-      slideshowSpeed: 7000,
-      animationSpeed: 600,
-      randomize: false,
-   });
+   // $('.flexslider').flexslider({
+   //    namespace: "flex-",
+   //    controlsContainer: ".flex-container",
+   //    animation: 'slide',
+   //    controlNav: true,
+   //    directionNav: false,
+   //    smoothHeight: true,
+   //    slideshowSpeed: 7000,
+   //    animationSpeed: 600,
+   //    randomize: false,
+   // });
 
 /*----------------------------------------------------*/
 /*	contact form
 ------------------------------------------------------*/
 
-   $('form#contactForm button.submit').click(function() {
+   // $('form#contactForm button.submit').click(function() {
 
-      $('#image-loader').fadeIn();
+   //    $('#image-loader').fadeIn();
 
-      var contactName = $('#contactForm #contactName').val();
-      var contactEmail = $('#contactForm #contactEmail').val();
-      var contactSubject = $('#contactForm #contactSubject').val();
-      var contactMessage = $('#contactForm #contactMessage').val();
+   //    var contactName = $('#contactForm #contactName').val();
+   //    var contactEmail = $('#contactForm #contactEmail').val();
+   //    var contactSubject = $('#contactForm #contactSubject').val();
+   //    var contactMessage = $('#contactForm #contactMessage').val();
 
       
       
-      return false;
-   });
+   //    return false;
+   // });
 
 
 });
